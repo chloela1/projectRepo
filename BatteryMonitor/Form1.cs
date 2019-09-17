e954c481ec4017ac1407758c908c761d07b142a5
Chloe Lee <laiguillechloe@gmail.com>
2019-09-17 13:58:34 +0400

    Refresh Button Added

--- BatteryMonitor/Form1.cs
+++ BatteryMonitor/Form1.cs
@@ -16,5 +16,10 @@ namespace BatteryMonitor
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
